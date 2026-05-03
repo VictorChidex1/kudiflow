418cc9ec23a4cb1e5c1a853db45b11cbb1de3bf4
“VictorChidex1” <Donchid.online@gmail.com>
2026-05-03 01:22:29 +0100

    fixed chat widget positioning

--- src/components/chat/AIChatWidget.tsx
+++ src/components/chat/AIChatWidget.tsx
@@ -184,7 +184,11 @@ export function AIChatWidget() {
       </AnimatePresence>
 
       {/* Floating Toggle Button */}
-      <div className={`fixed bottom-6 right-6 z-[60] ${isOpen ? 'hidden sm:block' : ''}`}>
+      <div
+        className={`fixed bottom-24 lg:bottom-6 right-4 lg:right-6 z-[60] ${
+          isOpen ? "hidden sm:block" : ""
+        }`}
+      >
         <div className="relative">
           {/* Pulsing ring effect when closed */}
           {!isOpen && (
